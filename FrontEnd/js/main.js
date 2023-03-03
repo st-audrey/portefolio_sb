d10252d34357aac4d09f52283e6556aa6a0e56aa
st-audrey <st_audrey@outlook.fr>
2023-03-03 17:36:31 +0100

    get categories form api

--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -3,14 +3,24 @@ function getWorks(){
         return response.json();
 
     }).then(function (data) {
-        console.log(data);
         dispatchWorks(data);
 
     }).catch(function (err) {
-        console.warn('Something went wrong.', err);
+        console.warn('Something went wrong with works', err);
     });
 }
 
+function getCategories(){
+    fetch('http://localhost:5678/api/categories').then(function (response) {
+        return response.json();
+
+    }).then(function (data) {
+        createFilters(data);
+
+    }).catch(function (err) {
+        console.warn('Something went wrong with categories', err);
+    });
+}
 
 function dispatchWorks(data){
     let gallery = document.getElementById('portfolio').getElementsByClassName('gallery')[0];
@@ -29,4 +39,56 @@ function dispatchWorks(data){
     });
 }
 
+function createFilters(data){
+
+    let portfolio = document.getElementById('portfolio');
+    let gallery = document.getElementById('portfolio').getElementsByClassName('gallery')[0];
+    let newContainer = document.createElement('div');  
+    newContainer.style.width = '50%';
+    newContainer.style.margin = '50px auto 50px';
+    newContainer.style.display = 'flex';
+    newContainer.style.flexDirection = 'row';
+    newContainer.style.justifyContent = 'space-around';
+    
+    newContainer.setAttribute('id', 'filters-container');
+
+    portfolio.insertBefore(newContainer, gallery);
+
+    let filtersContainer = document.getElementById('filters-container');
+
+    let allFilters = new Set();
+    allFilters.add("Tous");
+    for( i = 0 ; i < data.length ; i++ ){
+        allFilters.add(data[i].name);
+    }
+
+    allFilters.forEach(item => {
+        let filter = document.createElement('button');
+        filter.style.border = '1px solid #1D6154';
+        filter.style.backgroundColor = 'white';
+        filter.style.padding = '5px 17px 5px 17px';
+        filter.style.borderRadius = '40px';
+        filter.style.fontFamily = 'Syne';
+        filter.style.fontSize = '16px';
+        filter.style.color = '#1D6154';
+        filter.style.cursor = 'pointer';
+
+        filter.addEventListener("mouseover", (event) => {
+            filter.style.color = 'white';
+            filter.style.backgroundColor = '#1D6154';
+        });
+        
+        filter.addEventListener("mouseout", (event) => {
+            filter.style.color = '#1D6154';
+            filter.style.backgroundColor = 'white';
+        });
+        
+        filter.innerHTML = item;
+        filtersContainer.append(filter);
+    });   
+
+}
+
+
 getWorks();
+getCategories();
