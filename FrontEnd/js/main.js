4444eb6460636de5f28f041d450258339443f577
st-audrey <st_audrey@outlook.fr>
2023-03-06 11:20:33 +0100

    create gallery modal for edition

--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -156,6 +156,78 @@ function createFilters(data){
 
 }
 
+let modal = null;
+const focusableSelector = 'button, a, input';
+let focusables = [];
+let previouslyFocusedElement = null;
+
+const openModal = function (e) {
+    e.preventDefault();
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    modal = document.querySelector(e.target.getAttribute('href'));
+    focusables = Array.from(modal.querySelectorAll(focusableSelector));
+    previouslyFocusedElement = document.querySelector(':focus');
+    focusables[0].focus();
+    modal.style.display = null;
+    modal.removeAttribute('aria-hidden');
+    modal.setAttribute('aria-modal', 'true');
+    modal.addEventListener('click', closeModal);
+    modal.querySelector('.edition-close-modal-btn').addEventListener('click', closeModal);
+    modal.querySelector('.edition-modal-stop').addEventListener('click', stopPropagation);
+}
+
+const closeModal = function (e){
+    if ( modal === null ) return;
+    e.preventDefault();
+    if (previouslyFocusedElement !== null) previouslyFocusedElement.focus();
+    modal.style.display = 'none';
+    modal.setAttribute('aria-hidden', 'true');
+    modal.removeAttribute('aria-modal');
+    modal.removeEventListener('click', closeModal);
+    modal.querySelector('.edition-close-modal-btn').removeEventListener('click', closeModal);
+    modal.querySelector('.edition-modal-stop').removeEventListener('click', stopPropagation);
+    modal = null;
+}
+
+const stopPropagation = function (e) {
+    e.stopPropagation();
+}
+
+const focusInModal = function (e){
+    e.preventDefault();
+    let index = focusables.findIndex(f => f === modal.querySelector(':focus'));
+    if (e.shiftKey === true){
+        index--; 
+    }else{
+        index++;
+    }
+    
+    if(index >= focusables.length){
+        index = 0;
+    }
+    if(index < 0){
+        index = focusables.length -1;
+    }
+
+
+    focusables[index].focus();
+}
+
+document.querySelectorAll('.edition-modal').forEach(item =>{
+    item.addEventListener('click', openModal);
+})
+
+window.addEventListener('keydown', function (e) {
+    if( e.key === "Escape" || e.key === "Esc"){
+        closeModal(e);
+    }
+    if (e.key === 'Tab' && modal !== null) {
+        focusInModal(e);
+    }
+})
+
+
 checkLoggedIn();
 checkAuthorized();
     
