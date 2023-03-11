6301d68671981fb26c1e23ffa11244a59228459d
st-audrey <st_audrey@outlook.fr>
2023-03-11 21:58:07 +0100

    fix login error

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,34 +1,39 @@
 async function logUser(e){
-  e.preventDefault();
 
-  let error = document.getElementById("error");
-  let user = {
-      email : document.getElementById("email_login").value,
-      password : document.getElementById("password_login").value
-  }
-  
-  let response = await fetch('http://localhost:5678/api/users/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify(user)
-  });
-  
-  let result = await response.json();
+    e.preventDefault()
 
-  if(result.userId){
+    let error = document.getElementById("error");
+    let user = {
+        email : document.getElementById("email_login").value,
+        password : document.getElementById("password_login").value
+    }
 
-    localStorage.setItem("token", result.token)
-    localStorage.setItem("user_id", result.userId)
-   
-    window.location.href="index.html";
+    await fetch('http://localhost:5678/api/users/login', { 
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+            body: JSON.stringify(user)})
+            .then((response) => {
+                if (response.status == 404 || response.status == 401 || response.message) {
+                    error.innerHTML = "Erreur dans l\'identifiant ou le mot de passe"
+                    throw new Error("Bad response from server")
+        }
+            return response.json()
+        }).then((response) => {
     
+            localStorage.setItem("token", response.token)
+            localStorage.setItem("user_id", response.userId)
+            
+            window.location.href="index.html";
 
-  }else if(result.message){
-    error.innerHTML = "Erreur dans l\'identifiant ou le mot de passe"
-  }
+    }).catch((error) => {
+        // console.log(error)
+    });
 }
+
+
+
 
 
 
